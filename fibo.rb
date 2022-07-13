de8cc5f9cb2d4cbd8806c83cdbbe144c53eaaba8
AnnaGaskell <annalilygaskell@gmail.com>
2022-07-13 11:38:33 -0400

    final function added

--- fibo.rb
+++ fibo.rb
@@ -10,3 +10,14 @@ def fibs(number, result=[])
     end
     result
 end
+
+def fibs_rec(number)
+    if number.zero?
+        return [0]
+    if number == 1
+        return [0,1]
+    
+    result = fibs_rec(number - 1)
+    result << result[-1] + result[-2]
+end
+    
